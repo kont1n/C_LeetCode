206adec2db5b4d249a80dcb0bfad2f9db2a442b2
Konstantin Lapshin <lapshin.k@gmail.com>
2023-12-17 16:02:23 +0300

    twoSum

---
+++ Two Sum.cpp
@@ -0,0 +1,32 @@
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+/**
+Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+Example 2:
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+
+Example 3:
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ */
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> answer;
+        for (int i = 0; i < nums.size(); i++) {
+            for (int j = i+1; j < nums.size(); j++) {
+                if (target == nums[i]+nums[j]) {
+                    answer = {i, j};
+                    return answer;
+                }
+            }
+        };
+        return answer;
+    }
+};
